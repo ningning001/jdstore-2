8461d23e0c2231517c3b582289128bc08e2ffb7c
liangning001 <liangning001@gmail.com>
2017-05-13 14:23:51 +0800

    only admin can access backend panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 u = User.new
-u.email = "12@t12"           
+u.email = "12@t12"
 
 u.password = "111111"
 
 u.password_confirmation = "111111"
+
+u.is_admin = true
+u.save
+u = User.new
+u.email = "123@123.com"
+
+u.password = "123456"
+
+u.password_confirmation = "123456"   
 
 u.is_admin = true
 u.save
