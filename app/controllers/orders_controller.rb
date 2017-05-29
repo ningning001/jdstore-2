87463bbbbc2feb7413336e43e5064a5a00b7ab9a
liangning001 <liangning001@gmail.com>
2017-05-29 18:51:08 +0800

    add orders show function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
-
+  def index
+     @orders = current_user.orders
+   end
    def create
      @order = Order.new(order_params)
      @order.user = current_user
@@ -15,8 +17,6 @@ class OrdersController < ApplicationController
                 product_list.quantity = cart_item.quantity
                 product_list.save
               end
-
-
        redirect_to order_path(@order)
      else
        render 'carts/checkout'
@@ -26,9 +26,7 @@ class OrdersController < ApplicationController
      @order = Order.find(params[:id])
      @product_lists = @order.product_lists
    end
-
    private
-
    def order_params
      params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address)
    end
